ea5c58a2d6bf0d92692d087ecd87cd4921887847
Farazaulia <raz.muhammad14@gmail.com>
2019-11-14 03:52:35 +0700

    Initial Commit

---
+++ Tasks/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tasks
+//
+//  Created by Faraz Muhammad Aulia on 14/11/19.
+//  Copyright © 2019 Faraz Muhammad Aulia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
